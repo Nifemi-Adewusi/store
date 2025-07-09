02cde9a18ca4cf5b7903c676930f0c60e7edbecd
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-09 16:19:10 +0100

    Update CartOverview Styles

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-800 text-stone-200 uppercase">
-      <p className="text-stone-300 font-semibold">
+    <div className="bg-stone-800 px-4 py-4 text-stone-200 uppercase sm:px-6 flex justify-between items-center">
+      <p className="text-stone-300 font-semibold gap-5 text-sm md:text-base flex space-x-4">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
-      <Link to="/cart">Open cart &rarr;</Link>
+      <Link className="" to="/cart">Open cart &rarr;</Link>
     </div>
   );
 }
