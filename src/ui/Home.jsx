9ac0071d5c437a344789d44c380ce0bf61834a17
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-09 16:23:49 +0100

    Update Home Styles

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,13 +1,16 @@
+import CreateUser from "../features/user/CreateUser";
+
 function Home() {
   return (
-    <div>
-      <h1 className="text-center  text-xl">
+    <div className="text-center my-10 sm:my-16">
+      <h1 className="mb-8 md:text-3xl  text-xl font-semibold px-4 sm:px-0">
         The best pizza.
         <br />
         <span className="text-yellow-500">
           Straight out of the oven straight to you.
         </span>
       </h1>
+      <CreateUser/>
     </div>
   );
 }
