ba9d728b277c94c86469abc490af9266c49a5243
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-09 12:46:24 +0100

    Update Headers

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import UserName from "../features/user/UserName";
 
 function Header() {
   return (
     <header className="bg-yellow-500 uppercase">
       <Link className="tracking-widest" to="/">Fast React Pizza Company</Link>
       <SearchOrder />
-      <p>Nifemi</p>
+      {/* <p>Nifemi</p> */}
+      <UserName/>
     </header>
   );
 }
