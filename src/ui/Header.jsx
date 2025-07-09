d179517fa9bb08eeb22674764df75ec164951a6c
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-09 16:23:01 +0100

    Update Header Styles

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import UserName from "../features/user/UserName";
 
 function Header() {
   return (
-    <header className="bg-yellow-500 uppercase">
+    <header className="bg-yellow-500 uppercase md:text-2xl  px-4 py-4 border-b  border-stone-200 gap-3 items-center flex flex-col sm:px-6 sm:flex-row sm:justify-around">
       <Link className="tracking-widest" to="/">Fast React Pizza Company</Link>
       <SearchOrder />
       {/* <p>Nifemi</p> */}
