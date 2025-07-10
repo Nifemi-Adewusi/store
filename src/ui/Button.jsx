2b8ea9f9ae5b377bf30ec2af19774b185ea3dd21
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-07-10 16:39:47 +0100

    Update Button Flexibility

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
-function Button({ isSubmitting }) {
+function Button({ disabled, children }) {
   return (
     <button
       className="bg-yellow-400 font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-      disabled={isSubmitting}
+      disabled={disabled}
     >
-      {isSubmitting ? "Placing Orders..." : "Order Now"}
+      {children}
     </button>
   );
 }
